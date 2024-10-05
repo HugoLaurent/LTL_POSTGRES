f4f35d8f7035f404dd9045fa3ea578ebfd3a9671
HugoLaurent <lauhugopro@gmail.com>
2024-10-05 16:11:37 +0200

    Add configuration for Adonis.js app

---
+++ ecosystem.config.ts
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'adonis-app',
+      script: 'node',
+      args: 'ace serve --watch',
+      watch: true,
+      env: {
+        NODE_ENV: 'development',
+        HOST: '0.0.0.0',
+        PORT: '3333',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        HOST: '0.0.0.0',
+        PORT: '3333',
+      },
+    },
+  ],
+}
