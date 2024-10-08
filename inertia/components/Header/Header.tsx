921b85b0d48f45edeaf9b014124d2a7f4fb3cfb1
HugoLaurent <90903947+HugoLaurent@users.noreply.github.com>
2024-10-08 17:21:26 +0200

    Update Header.tsx

--- inertia/components/Header/Header.tsx
+++ inertia/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import '~/components/Header/header-style.css'
 export default function Header() {
   return (
     <nav className="nav-container">
-      <h1 className="nav-container__title">Lache Ta Test'</h1>
+      <h1 className="nav-container__title">Lache Ta Loc'</h1>
       <ul className="nav-container__buttons">
         <li className="nav-container__button">Accueil</li>
         <li className="nav-container__button">Trouver la Loc'</li>
